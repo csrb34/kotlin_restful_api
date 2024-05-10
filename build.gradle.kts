63c61418c31e448e7b59afa23b9136ebb0af0972
Carla San Roman <carla.s@thoughtworks.com>
2024-05-10 12:59:55 +0200

    add default files

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
+
+
+Add ssh key to agent
+ssh-add --apple-use-keychain ~/.ssh/github-primerio
