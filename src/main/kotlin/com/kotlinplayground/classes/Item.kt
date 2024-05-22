c1f7189ca7a51c3c753724b1d9c7b5569137a3f6
Carla San Roman <carla.s@thoughtworks.com>
2024-05-22 12:17:33 +0200

    getters and setters

--- src/main/kotlin/com/kotlinplayground/classes/Item.kt
+++ src/main/kotlin/com/kotlinplayground/classes/Item.kt
@@ -2,6 +2,21 @@ package org.example.com.kotlinplayground.classes
 
 class Item() {
     var name: String = ""
+    var price: Double = 0.0
+//    get() = field
+    get() {
+        println("Inside getter")
+        return field
+    }
+
+    set(value) {
+        println("Inside setter")
+        if (value >= 0.0){
+            field = value
+        }else{
+            throw IllegalArgumentException("Negative price is not allowed")
+        }
+    }
 
     constructor(_name: String): this(){ // Secondary constructor
         name = _name
@@ -11,7 +26,12 @@ class Item() {
 fun main() {
 
     val item = Item( _name="Phone")
-    println(item.name)
-    item.name = "Phone case"
-    println(item.name)
+//    println(item.name)
+//    item.name = "Phone case"
+//    println(item.name)
+    println(item.price)
+    item.price = 10.0
+//    item.price = -10.0
+    println(item.price)
+
 }
